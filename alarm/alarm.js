4b02457d1d24ab93c0de282a72ced9ad5abf820c
Swagat <swagatmitrabhattacharya18@gmail.com>
2023-05-12 22:40:24 +0530

    never gonna give you up

--- alarm/alarm.js
+++ alarm/alarm.js
@@ -1,64 +1,80 @@
-let time = document.querySelector('input');
-let div = document.querySelector('.text');
-let button = document.querySelector('button');
-let alarm = document.querySelector('audio');
+let time = document.querySelector("input");
+let div = document.querySelector(".text");
+let button = document.querySelector("button");
+let alarm = document.querySelector("audio");
 
-let target; 
+let target;
 
 const timer = function () {
-    button.addEventListener('click', () => {
-        let today = new Date(); 
-        target = time.value.split(":");
-        if (target.length == 1) return; 
-        
-        let targetTime = new Date(today.getFullYear(), today.getMonth(), today.getDate(), target[0], target[1]);
-        
-        function count() {
-            let now = new Date()
-            let inSeconds = (targetTime - now) / 1000
-            console.log(inSeconds.toFixed(0))
-            
-            if (inSeconds < 0) {
-                clearInterval(go);
-                alarm.play();
-                div.innerHTML = `<button>Snooze</button>
-                                 <button>Dismiss</button>`
+  button.addEventListener("click", () => {
+    let today = new Date();
+    target = time.value.split(":");
+    if (target.length == 1) return;
 
-                const buttons = div.querySelectorAll('button');
-                const snooze = buttons[0];
-                const dismiss = buttons[1];
+    let targetTime = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate(),
+      target[0],
+      target[1]
+    );
 
-                snooze.addEventListener('click', () => {
-                    alarm.pause()
-                    setTimeout(() => {
-                        alarm.play()
-                    }, 180000)
-                })
+    function count() {
+      let now = new Date();
+      let inSeconds = (targetTime - now) / 1000;
+      console.log(inSeconds.toFixed(0));
 
-                dismiss.addEventListener('click', () => {
-                    alarm.pause()
-                    alarm.currentTime = 0;
-                    div.innerHTML = ''
-                })
-            }
-        }
-        
-        const go = setInterval(count, 1000);
+      if (inSeconds < 0) {
+        clearInterval(go);
+        alarm.play();
+        div.innerHTML = `<button>Snooze</button>
+                                 <button>Dismiss</button>`;
 
-        const seconds = (targetTime - today) / 1000;
-        const hoursLeft = Math.floor(seconds / 3600) % 24;
-        const minutesLeft = Math.floor(seconds / 60) % 60;
+        const buttons = div.querySelectorAll("button");
+        const snooze = buttons[0];
+        const dismiss = buttons[1];
 
-        button.style.backgroundColor = 'lightgreen'; 
-        button.style.borderRadius = '0.3rem';
+        snooze.addEventListener("click", () => {
+          alarm.pause();
+          div.style.display = "none";
+          
+          setTimeout(() => {
+            div.textContent = 'Snoozed for 3 minutes'
+          }, 3000)
 
-        div.textContent = `Alarm set for ${hoursLeft} hour${hoursLeft > 1 || hoursLeft == 0 ? 's': ''} and ${minutesLeft} minute${minutesLeft > 1 || minutesLeft == 0 ? 's': ''} from now`
+          setTimeout(() => {
+            alarm.play();
+            div.style.display = "block";
+          }, 180000);
+        });
 
-        setTimeout(() => {
-            div.textContent = ''
-        }, 5000)
-        
-    })
-}
+        dismiss.addEventListener("click", () => {
+          alarm.pause();
+          alarm.currentTime = 0;
+          div.innerHTML = "";
+        });
+      }
+    }
 
-timer()
+    const go = setInterval(count, 1000);
+
+    const seconds = (targetTime - today) / 1000;
+    const hoursLeft = Math.floor(seconds / 3600) % 24;
+    const minutesLeft = Math.floor(seconds / 60) % 60;
+
+    button.style.backgroundColor = "lightgreen";
+    button.style.borderRadius = "0.3rem";
+
+    div.textContent = `Alarm set for ${hoursLeft} hour${
+      hoursLeft > 1 || hoursLeft == 0 ? "s" : ""
+    } and ${minutesLeft} minute${
+      minutesLeft > 1 || minutesLeft == 0 ? "s" : ""
+    } from now`;
+
+    setTimeout(() => {
+      div.textContent = "";
+    }, 5000);
+  });
+};
+
+timer();
