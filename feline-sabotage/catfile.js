194c2279256aab0e6e62172c9917bccf46e750c6
Swagat <swagatmitrabhattacharya18@gmail.com>
2023-05-13 15:51:42 +0530

    nya nya

--- feline-sabotage/catfile.js
+++ feline-sabotage/catfile.js
@@ -1,5 +1,6 @@
 let images = Array.from(document.getElementsByTagName("img"));
 let allheaders = Array.from(document.getElementsByTagName("h2"));
+let anchorTags = Array.from(document.getElementsByTagName("a"))
 
 Promise.all(
   images.map((image) =>
@@ -13,6 +14,10 @@ Promise.all(
   )
 );
 
-for (let link of allheaders) {
-    link.textContent = 'you have been catjacked'
+for (let header of allheaders) {
+    header.textContent = 'you have been catjacked'
+}
+
+for (let link of anchorTags) {
+     link.href = 'https://www.youtube.com/watch?v=QH2-TGUlwu4'
 }
