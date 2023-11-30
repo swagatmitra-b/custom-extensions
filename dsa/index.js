65b0ed36d772c01db16972642a038d98392417c3
Swagat <swagatmitrabhattacharya18@gmail.com>
2023-12-01 00:03:37 +0530

    initial commit

---
+++ dsa/index.js
@@ -0,0 +1,70 @@
+import { qs } from "./questions.js";
+
+const sectionDone = document.querySelector(".done");
+const sectionRem = document.querySelector(".remaining");
+const done = document.querySelector(".donumber");
+const tbd = document.querySelector(".remainumber");
+
+let current = [];
+
+const loader = () => {
+  done.textContent = `(${current.completed.length})`;
+  tbd.textContent = `(${current.remaining.length})`;
+  const boxes = Array.from(document.querySelectorAll(".check"));
+  boxes.forEach((question) => {
+    question.addEventListener("change", (e) => {
+      if (e.target.checked) {
+        const qt = question.nextElementSibling.textContent;
+        current.completed.push({
+          name: qt,
+          link: current.remaining.find((q) => q.name == qt).link,
+        });
+        current.remaining = current.remaining.filter((q) => q.name != qt);
+        done.textContent = `(${current.completed.length})`;
+        tbd.textContent = `(${current.remaining.length})`;
+        localStorage.setItem("questions", JSON.stringify(current));
+        domLoad();
+      } else {
+        const qt = question.nextElementSibling.textContent;
+        current.remaining.push({
+          name: qt,
+          link: current.completed.find((q) => q.name == qt).link,
+        });
+        current.completed = current.completed.filter((q) => q.name != qt);
+        localStorage.setItem("questions", JSON.stringify(current));
+        domLoad();
+      }
+    });
+  });
+};
+
+const questionLoader = () => {
+  console.log(current.completed, current.remaining);
+  for (let { name, link } of current.completed) {
+    sectionDone.innerHTML += `
+            <div class="box">
+              <input type="checkbox" class="check" checked> <a href="${link}">${name}</a>
+            </div>
+          `;
+  }
+  for (let { name, link } of current.remaining) {
+    sectionRem.innerHTML += `
+            <div class="box">
+              <input type="checkbox" class="check"> <a href="${link}">${name}</a>
+            </div>
+          `;
+  }
+};
+
+const domLoad = () => {
+  let questions = JSON.parse(localStorage.getItem("questions")) || [];
+  if (questions.length == 0) current = qs;
+  else current = questions;
+  console.log(current);
+  sectionDone.innerHTML = "";
+  sectionRem.innerHTML = "";
+  questionLoader();
+  loader();
+};
+
+document.addEventListener("DOMContentLoaded", domLoad);
---
+++ dsa/questions.js
@@ -0,0 +1,21 @@
+export const qs = {
+  remaining: [
+    {
+      name: "Two Sum",
+      link: "https://leetcode.com/problems/two-sum",
+    },
+    {
+      name: "Merge Two Sorted Lists",
+      link: "https://leetcode.com/problems/merge-two-sorted-lists",
+    },
+    {
+      name: "Partition Equal Subset Sum",
+      link: "https://leetcode.com/problems/partition-equal-subset-sum",
+    },
+    {
+      name: "LRU Cache",
+      link: "https://leetcode.com/problems/lru-cache",
+    },
+  ],
+  completed: [],
+};
